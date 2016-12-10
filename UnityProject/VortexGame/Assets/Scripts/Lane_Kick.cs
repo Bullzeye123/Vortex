32df9ccffea725a9994fa8b20db34f764e409dc1
Pascal Möller <Pascal Mller>
2016-12-10 14:45:48 +0100

    Unity Add, Rename Folders

---
+++ UnityProject/VortexGame/Assets/Scripts/Lane_Kick.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane_Kick : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        
+
+        
+	}
+
+    private void switchGadgetPosition()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            anim.SetBool("Kick", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            anim.SetBool("Kick", false);
+        }
+    }
+
+    private void performAction()
+    {
+        if (Animator.StringToHash("wait") == anim.GetCurrentAnimatorStateInfo(0).tagHash && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Kick", true);
+        }
+        if (Animator.StringToHash("kick") == anim.GetCurrentAnimatorStateInfo(0).tagHash)
+        {
+            anim.SetBool("Kick", false);
+        }
+    }
+}
---
+++ UnityProject/VortexGame/Assets/Scripts/TiltWaver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiltWaver : MonoBehaviour {
+
+    public float tilt;
+    public float speed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(-moveHorizontal, 0.0f, -moveVertical);
+
+        rb.velocity = movement * speed;
+
+        rb.rotation = Quaternion.Euler(0.0f, rb.velocity.x * tilt, rb.velocity.x * tilt);
+    }
+}
