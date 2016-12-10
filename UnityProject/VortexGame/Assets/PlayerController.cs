d27ca1e9bfee92952e1379d9f4c496cf2b9c6213
Pascal Möller <Pascal Mller>
2016-12-10 18:25:56 +0100

    Working !!!!!!!!!

---
+++ UnityProject/VortexGame/Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject Player;
+
+    public GameObject Engine1;
+    public GameObject Engine2;
+    public GameObject LaneKick;
+
+    public Transform positionLeft;
+    public Transform positionRight;
+    public Transform positionBack;
+
+    private GameObject engine;
+    private GameObject punch;
+
+    // Use this for initialization
+    void Start () {
+        changePowerUps(Engine1, positionBack, ref engine);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            changePowerUps(LaneKick, positionLeft, ref punch);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(engine.name);
+            switch (engine.name)
+            {
+                case "Engine1(Clone)":
+                    changePowerUps(Engine2, positionBack, ref engine);
+                    break;
+                case "Engine2(Clone)":
+                    changePowerUps(Engine1, positionBack, ref engine);
+                    break;
+            }
+        }
+    }
+
+    private void changePowerUps(GameObject obj,Transform pose, ref GameObject dst)
+    {
+        if(dst != null)
+        {
+            Destroy(dst.gameObject);
+        }
+
+        dst = Instantiate(obj, pose.position, pose.rotation);
+        dst.tag = pose.tag;
+        dst.transform.parent = Player.transform;
+    }
+}
---
+++ UnityProject/VortexGame/Assets/Scripts/AddObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddObject : MonoBehaviour {
+
+    public GameObject powerUp;
+
+    public Transform postionLeft;
+    public Transform postionRight;
+    public Transform postionBack;
+
+    private bool posLeft;
+    private bool posRight;
+    private bool posBack;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(transform.position);
+        posLeft = true;
+        posRight = true;
+        posBack = true;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        // If Button is pressed spawn Object
+        if (Input.GetKeyDown(KeyCode.A) && posLeft)
+        {
+            Instantiate(powerUp, postionLeft.position, postionLeft.rotation);
+            posLeft = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && posRight)
+        {
+            Instantiate(powerUp, postionRight.position, postionRight.rotation);
+            posRight = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && posBack)
+        {
+            Instantiate(powerUp, postionBack.position, postionBack.rotation);
+            posBack = false;
+        }
+
+    }
+
+}
