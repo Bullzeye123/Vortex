3ee30f4224c83fd1e2da27684566ec53a2c5562c
Florian Sprang <florian.sprang@tum.de>
2016-12-10 10:58:30 +0100

    Created Unity Project

---
+++ UnityProject/VortexGame/Assets/Scripts/DestroyGameObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGameObject : MonoBehaviour
+{
+
+    public float destroyAfter;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (destroyAfter < 0)
+	    {
+	        destroy();
+	    }
+	    destroyAfter -= Time.deltaTime;
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            destroy();
+        }
+    }
+
+    void destroy()
+    {
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ UnityProject/VortexGame/Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = System.Random;
+
+public class RandomSpawn : MonoBehaviour
+{
+    private static Random r;
+    public float spawntime;
+    public bool laneDisabled;
+
+    public int ran_Low;
+    public int ran_High;
+
+    private String[] tagList;
+
+    public GameObject tree, box, car;
+
+    private Hashtable obstacles;
+    private GameObject[] objectList;
+
+    void Start()
+    {
+        r=new Random();
+
+        tagList = new string[] {"Tree", "Box", "Car"};
+
+        objectList = new GameObject[] {tree,box,car};
+
+        
+    }
+
+    void Update()
+    {
+        if (spawntime < 0)
+        {
+            int ran = randomNumber(0, 3);
+            GameObject newObject = Instantiate(objectList[ran], transform.position, Quaternion.Euler(0, 0, 0));
+            newObject.tag = tagList[(int)randomNumber(0, 2)];
+            spawntime = randomNumber(ran_Low, ran_High);
+            Debug.Log(spawntime);
+        }
+        else
+        {
+            spawntime -= Time.deltaTime;
+        }
+    }
+
+    private int randomNumber(int low, int high)
+    {
+        
+        return r.Next(low, high);
+    }
+}
